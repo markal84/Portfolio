f92f7f4a2c46893eda5086b65f310c242a11246f
Mar Kal <marc@example.com>
2023-06-23 18:07:43 +0200

    feat: add pagination to blog page

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
 import BlogArticle from './BlogArticle';
-import { Grid } from '@mui/material';
+import { Grid, Pagination, PaginationItem, Link } from '@mui/material';
 
 export default function BlogList(props) {
+  const { articles } = props;
+  const articlesPerPage = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(articles.length / articlesPerPage);
+  const startIndex = (currentPage - 1) * articlesPerPage;
+  const endIndex = startIndex + articlesPerPage;
+  const displayedArticles = articles.slice(startIndex, endIndex);
+
+  function handlePageChange(event, page) {
+    setCurrentPage(page);
+  }
+
   return (
     <Grid
       id="blogs"
@@ -12,7 +26,7 @@ export default function BlogList(props) {
       direction="row"
       sx={{ mt: 2, mb: 1 }}
     >
-      {props.articles.map((article) => {
+      {displayedArticles.map((article) => {
         const { id, date, title, desc, fullDesc, project, summary } = article;
         return (
           <BlogArticle
@@ -26,6 +40,12 @@ export default function BlogList(props) {
           />
         );
       })}
+      <Pagination
+        count={totalPages}
+        page={currentPage}
+        onChange={handlePageChange}
+        renderItem={(item) => <PaginationItem component={Link} to={`/blogs?page=${item.page}`} {...item} />}
+      />
     </Grid>
   );
 }
